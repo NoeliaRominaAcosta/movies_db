4fedc231261ea1519016a9b4aa13333a0325fe29
NoeliaRominaAcosta <noeliaromina20@gmail.com>
2022-07-09 16:19:18 -0300

    finished work

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,6 +1,6 @@
 const { Op } = require('sequelize');
 const db = require('../database/models');
-
+const moment = require('moment');
 //lee el archivo index que tiene un module.exports
 
 module.exports = {
@@ -42,11 +42,95 @@ module.exports = {
                 rating: {[db.Sequelize.Op.gte] : 8}
             },
             order : [
-                ['rating', DESC]
-                ['awards', DESC]
+                ['rating', 'DESC']
+                ['awards', 'DESC']
             ]
             .then(movies => res.render('recommendedMovies',{movies}))
             .catch(error => console.log(error))
         })
+    },
+      add: function (req, res) {
+        //acceder a genre qye esta en modelos
+        db.Genre.findAll({
+            order : [
+                ['name','ASC']
+            ]//las ordena en orden alfabetico
+        })
+            .then(genres => {
+               /*  return res.send(genres) */ //lo hacemos para ver lo que llega para ver como te esta mandando
+                return res.render('moviesAdd', {
+                    genres
+                }) 
+            }) //cuando lo encuentra renderiza la vsta
+        .catch(error => console.log(error));
+    },
+    create: function (req, res) {
+        const {title,awards, release_date,genre_id,rating,length} = req.body
+       db.Movie.create({
+        title : title.trim(),
+        awards : +awards,
+        release_date,
+        genre_id : +genre_id,
+        rating : +rating,
+        length: +length
+       })//el create me da el elemento recien creado 
+       .then(movie => {
+        console.log(movie);
+        return res.redirect('/movies/detail/' + movie.id)
+       })
+       .catch(error => console.log(error))
+    },
+    edit: function(req, res) {
+       const movie = db.Movie.findByPk(req.params.id)
+       const genres = db.Genre.findAll({
+        order : ['name']
+       })
+       Promise.all([movie,genres]) //le paso todas las promesas
+        //necesito mandar tambien el genero por eso uso promise all
+        .then(([movie,genres]) => {//al then le paso la promise all
+            return res.render('moviesEdit',{
+                Movie : movie,
+                release_date : moment(movie.release_date).format('YYY-MM-DD'),//hay que cambiar el formato porque en sql es diferente
+                genres
+            })
+        })
+        .catch(error => console.log(error))
+    },
+    update: function (req,res) {
+        // dos objetos : que queres editar, donde queres editarlo
+        const {title,awards, release_date,genre_id,rating,length} = req.body
+
+        db.Movie.update(
+            { 
+                title : title.trim(),
+                awards : +awards,
+                release_date,
+                genre_id : +genre_id,
+                rating : +rating,
+                length: +length
+
+            },
+            {
+                where : {
+                    id : req.params.id
+                }
+            }
+        ).then(() => res.redirect('/movies'))
+        .catch(error => console.log(error))
+
+    },
+    delete: function (req, res) {
+        db.Movie.findByPk(req.params.id)
+        .then(movie => res.render('moviesDelete',{
+            movie
+        })).catch(error => console.log(error))
+    },
+    destroy: function (req, res) {
+        db.movie.destroy({
+            where : {
+                id : req.params.id
+            }
+        }).then(() => res.redirect('/movies'))
+        .catch(error => console.log(error))
     }
 }
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const moviesController = require('../controllers/moviesController');
 
-router.get('/movies', moviesController.list);
-router.get('/movies/new', moviesController.new);
-router.get('/movies/recommended', moviesController.recomended);
-router.get('/movies/detail/:id', moviesController.detail);
-
+router
+    .get('/movies', moviesController.list)
+    .get('/movies/new', moviesController.new)
+    .get('/movies/recommended', moviesController.recomended)
+    .get('/movies/detail/:id', moviesController.detail)
+//crud
+    .get('/movies/add', moviesController.add)
+    .post('/movies/create', moviesController.create)
+    .get('/movies/edit/:id', moviesController.edit)
+    .put('/movies/update/:id', moviesController.update)
 
 module.exports = router;
